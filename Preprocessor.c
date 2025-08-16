444054075097ae954e5f894e80854a4b529e8560
AmolMane30 <maneamol3001@gmail.com>
2025-08-16 12:20:43 +0530

    Creation of Preprocessor.c

---
+++ Preprocessor.c
@@ -0,0 +1,27 @@
+//-------------------------------------------------------------------------------------------//
+//        Project Title: TOOLCHAIN_OF_C_C++_PROGRAMMING
+//        Description  : The .c or .cpp program is converted from human readable code to 
+//                       binary code for the execution of the program as computer only understands 
+//                       the binary language that is in the form of 0 and 1 only.
+//        Author       : Amol R. Mane
+//        Date         : 16 August, 2025
+//-------------------------------------------------------------------------------------------//
+
+#include<stdio.h>
+
+// User defined header file
+#define DOZEN 12
+
+int main()
+{
+  int No = DOZEN;
+  printf("Jay Ganesh...");
+  printf("Value of No is %d\n",No);
+  
+  return 0;
+}
+
+// compilation
+// gcc Preprocessor.c -save-temps -o Myexe
+// Myexe.exe for Windoes 
+// ./Myexe for Linux
